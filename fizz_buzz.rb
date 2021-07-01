f2ade5d2bcd8bf2c7767c22b0a7220003761bec7
EC2 Default User <ec2-user@ip-172-31-19-158.ec2.internal>
2021-07-01 05:02:43 +0000

    [Fix}2-8

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,12 +1,12 @@
 def fizz_buzz(number)
   if (number%3==0)&&(number%5==0)
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif (number%3==0)&&(number%5!=0)
-      puts "Fizz"
+      "Fizz"
   elsif (number%3!=0)&&(number%5==0)
-      puts "Buzz"
+      "Buzz"
   else
-    puts "#{number}"
+    number.to_s
   end
 end
 
